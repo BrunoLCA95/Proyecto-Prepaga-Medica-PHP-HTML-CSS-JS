e7da9460c7c5f0f4ab1979383edfe32128e1a35d
BrunoLCA95 <brunolcosimano95@gmail.com>
2021-09-06 21:11:57 -0300

    nuevas funciones js

--- js/nuevo.js
+++ js/nuevo.js
@@ -5,19 +5,28 @@ var bnb2 = document.getElementById("bnb2");
 
 var factura = document.getElementById("factura");
 var ocultarFS1 = document.getElementById("fsn");
+var ocultarRM1 = document.getElementById("ocultarRegN");
+
+
 
 function mostrarBnB() {
     bnb.style.display = "block";
     bnb1.style.display = "block";
     bnb2.style.display = "block";
-    factura.style.display = "none";
     ocultarFS1.style.display = "none";
+    ocultarRM1.style.display = "none";
+    factura.style.display = "none";
 }
 
 function ocultarFactura() {
     
     if (factura.style.display === "none") {
         factura.style.display = "block";
+        ocultarFS1.style.display = "none";
+        ocultarRM1.style.display = "none";
+
+
+
     } else {
         factura.style.display = "none";
     }
@@ -30,6 +39,10 @@ function ocultarFSN() {
     
     if (ocultarFS1.style.display === "none") {
         ocultarFS1.style.display = "block";
+        factura.style.display = "none";
+        ocultarRM1.style.display = "none";
+
+
     } else {
         ocultarFS1.style.display = "none";
     }
@@ -39,11 +52,15 @@ function ocultarFSN() {
 }
 
 function ocultarRegN() {
-    var z = document.getElementById("ocultarRegN");
-    if (z.style.display === "none") {
-        z.style.display = "block";
+    
+    if (ocultarRM1.style.display === "none") {
+        ocultarRM1.style.display = "block";
+        factura.style.display = "none";
+        ocultarFS1.style.display = "none";
+
+
     } else {
-        z.style.display = "none";
+        ocultarRM1.style.display = "none";
     }
     bnb.style.display = "none";
     bnb1.style.display = "none";
